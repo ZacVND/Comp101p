330b28ed9cf2cd828eb3568a1a7b3ab5034e05ee
ZacVND <zac.luong.15@ucl.ac.uk>
2015-10-18 00:11:09 +0100

    added questions 2.5 and 2.6 solutions

---
+++ week2/q2_5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int r = 0;
+	do {
+		if(r <1)
+		{
+			for(int c = 0; c <= 5-r; c++)
+			{
+				printf("*");
+			}
+		}
+		else{
+			for(int c = 0; c < r; c++)
+			{
+				printf(" ");
+			}
+				for(int c2 = 0; c2<= 5-r; c2++)
+				{
+					printf("*");
+				}
+				
+		}
+		printf("\n");
+		r++;
+	}while(r <= 5);
+	
+	return 0;
+}
